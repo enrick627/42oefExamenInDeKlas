2d57ea08347bb128887493664715e967baafb0c2
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2020-01-14 09:41:42 +0100

    1ste commit

--- oefExamenInDeKlas/Program.cs
+++ oefExamenInDeKlas/Program.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿//Enrick De Munter 5ITN nr3 14/1/2020
+/*
+ * ANALYSE
+ * =======
+ * VRAAG    voornaam
+ * VRAAG    famillienaam
+ * 
+ * HERHAAL  
+ *   VRAAG    res
+ *    ALS      res is geldig
+ *    DAN      
+ *   BEREKEN  aantalRes +1
+ *   BEREKEN  totaal + res
+ *    ALS      res < 50
+ *    DAN      aantaltekort + 1
+ *   BEREKEN  laagste
+ *    ALS      res < laagste
+ *    DAN      laagste = res
+ *   BEREKEN  hoogste
+ *    ALS     res > hoogste
+ *    DAN     hoogste = res
+ *   ANDERS   toon foutboodschap
+ * ZOLANG res niet leeg
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +36,42 @@ namespace oefExamenInDeKlas
     {
         static void Main(string[] args)
         {
+            //variabelen
+            string voornaam, famillienaam;
+            byte resultaat;
+
+            // VRAAG    voornaam
+            Console.WriteLine("wat is uw voornaam?: ");
+            voornaam = Console.ReadLine();
+            // VRAAG    famillienaam
+            Console.WriteLine("wat is uw achternaam?: ");
+            famillienaam = Console.ReadLine();
+
+            // HERHAAL  
+            while ()
+            {
+
+            }
+            //   VRAAG    res
+            //    ALS      res is geldig
+            //    DAN      
+            //   BEREKEN  aantalRes +1
+            //   BEREKEN  totaal + res
+            //    ALS      res < 50
+            //    DAN      aantaltekort + 1
+            //   BEREKEN  laagste
+            //    ALS      res < laagste
+            //    DAN      laagste = res
+            //   BEREKEN  hoogste
+            //    ALS     res > hoogste
+            //    DAN     hoogste = res
+            //   ANDERS   toon foutboodschap
+            // ZOLANG res niet leeg
+
+            //WACHTEN
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten");
+            Console.ReadKey();
         }
     }
 }
